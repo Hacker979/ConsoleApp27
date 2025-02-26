24c32d791378a8e10dd1e94360fcaef82ae5c744
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-02-26 09:17:43 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static int[] Reverse(int[] mas)
+        {
+            int b = 0;
+            int[] Newmas = new int[mas.Length];
+            for (int i = mas.Length - 1; i >= 0; i--)
+
+            {
+                Newmas[b] = mas[i];
+                b++;
+            }
+            return Newmas;
+        }
+        static void PrintMas(int[] mas)
+        {
+            foreach (int i in mas)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        static void Mas()
+        {
+            int[] mas = new int[5];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"mas[{i}] = ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            PrintMas(Reverse(mas));
+        }
+
+        static void Main(string[] args)
+        {
+            Mas();
+
+            Console.Read();
+        }
+    }
+}
